02f2fbe0719583d0404d4c52f7329865beab8dad
Jeremy Leung <jml230@pitt.edu>
2020-11-17 17:03:45 -0500

    fix so auxref_str is not bytestring

--- src/westpa/core/states.py
+++ src/westpa/core/states.py
@@ -52,9 +52,7 @@ class BasisState:
         for state in states:
             state_id_str = str(state.state_id) if state.state_id is not None else 'None'
             pcoord_str = str(list(state.pcoord))
-            auxref_str = str(state.auxref)
-            print(state.auxref)
-            print(auxref_str)
+            auxref_str = str(state.auxref) if type(auxref_str) is not bytes else ''
             fileobj.write(
                 fmt.format(
                     state=state,
