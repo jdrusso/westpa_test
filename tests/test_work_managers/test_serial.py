caf419c2d745825e2577514810d239cf318b202f
Jeremy Leung <jml230@pitt.edu>
2020-11-17 15:37:19 -0500

    remove test_work_manager for quicker debug

--- tests/test_work_managers/test_serial.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from westpa.work_managers.serial import SerialWorkManager
-from .tsupport import CommonWorkManagerTests
-
-
-class TestSerialWorkManager(unittest.TestCase, CommonWorkManagerTests):
-    def setUp(self):
-        self.work_manager = SerialWorkManager()
