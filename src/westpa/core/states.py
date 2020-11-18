04ebcf94bb7c74a9d6011b027687898963869e7a
Jeremy Leung <jml230@pitt.edu>
2020-11-18 16:48:20 -0500

    encode with state.label with utf-8

--- src/westpa/core/states.py
+++ src/westpa/core/states.py
@@ -20,7 +20,7 @@ class BasisState:
     '''
 
     def __init__(self, label, probability, pcoord=None, auxref=None, state_id=None):
-        self.label = str(label)
+        self.label = str(label, encoding="utf-8")
         self.probability = probability
         self.pcoord = np.atleast_1d(pcoord)
         self.auxref = auxref
