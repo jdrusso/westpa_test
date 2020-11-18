3fac4e3984cfacfd3a8222ccfd01e3228aee6500
Jeremy Leung <jml230@pitt.edu>
2020-11-18 16:49:09 -0500

    capitalize UTF-8

--- src/westpa/core/states.py
+++ src/westpa/core/states.py
@@ -20,7 +20,7 @@ class BasisState:
     '''
 
     def __init__(self, label, probability, pcoord=None, auxref=None, state_id=None):
-        self.label = str(label, encoding="utf-8")
+        self.label = str(label, encoding="UTF-8")
         self.probability = probability
         self.pcoord = np.atleast_1d(pcoord)
         self.auxref = auxref
